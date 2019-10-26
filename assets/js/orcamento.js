39e40e6cbb1d8060243af3b9b2590e8e8de5289a
LucasDambroz <lucas.dambroz10@gmail.com>
2019-10-26 12:36:23 -0300

    26-10

---
+++ assets/js/orcamento.js
@@ -0,0 +1,8 @@
+const arrayMateriais = [ 'Apontador','Canetinhas Sylvapen','Lapiseria','Caneta','Clips coloridos','Corretivo','Régua','Borracha','Tesoura'];
+
+const materialEscolar = document.getElementById("materialEscolar");
+
+arrayMateriais.forEach((material) => {
+    option = new Option(material, material.toLowerCase());
+    materialEscolar.options[materialEscolar.options.length] = option;
+  });
